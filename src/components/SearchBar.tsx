36150648efd287870b02865605b423aadb150ee0
DukeSpontaneous <eai_bat@tut.by>
2023-03-24 04:31:16 +0300

    Implementation of pagination.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Pagination } from "antd";
 import { swapi } from "../services/swapi";
 import { Character } from "../types/entities/Character";
 import { Starship } from "../types/entities/Starship";
@@ -15,21 +16,59 @@ export const SearchBar = () => {
   const [starshipResults, setStarshipResults] = useState<Starship[]>([]);
   const [loading, setLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [maxCount, setMaxCount] = useState(0);
 
-  const handleSearch = async () => {
+  const handleSearch = async (page: number) => {
+    console.log(`people/?search=${searchTerm}&page=${page}`);
     setLoading(true);
+    setCurrentPage(page);
     try {
-      await Promise.allSettled([
+      const counters = await Promise.allSettled([
         swapi
-          .get<SearchResult<Character>>(`people/?search=${searchTerm}`)
-          .then(({ data: { results } }) => setCharacterResults(results)),
+          .get<SearchResult<Character>>(
+            `people/?search=${searchTerm}&page=${page}`
+          )
+          .then(({ data: { results, count } }) => {
+            setCharacterResults(results);
+            return count;
+          })
+          .catch(() => {
+            setCharacterResults([]);
+            return 0;
+          }),
         swapi
-          .get<SearchResult<Planet>>(`planets/?search=${searchTerm}`)
-          .then(({ data: { results } }) => setPlanetResults(results)),
+          .get<SearchResult<Planet>>(
+            `planets/?search=${searchTerm}&page=${page}`
+          )
+          .then(({ data: { results, count } }) => {
+            setPlanetResults(results);
+            return count;
+          })
+          .catch(() => {
+            setPlanetResults([]);
+            return 0;
+          }),
         swapi
-          .get<SearchResult<Starship>>(`starships/?search=${searchTerm}`)
-          .then(({ data: { results } }) => setStarshipResults(results)),
-      ]);
+          .get<SearchResult<Starship>>(
+            `starships/?search=${searchTerm}&page=${page}`
+          )
+          .then(({ data: { results, count } }) => {
+            setStarshipResults(results);
+            return count;
+          })
+          .catch(() => {
+            setStarshipResults([]);
+            return 0;
+          }),
+      ]).then((results) =>
+        results
+          .filter(
+            (r): r is PromiseFulfilledResult<number> => r.status === "fulfilled"
+          )
+          .map(({ value }) => value)
+      );
+      setMaxCount(Math.max(0, ...counters));
     } catch (error) {
       console.error(error);
     }
@@ -43,31 +82,41 @@ export const SearchBar = () => {
         value={searchTerm}
         onChange={({ target: { value } }) => setSearchTerm(value)}
       />
-      <button onClick={handleSearch}>Search</button>
-      <div style={{ display: "flex" }}>
-        {loading && <p>Loading...</p>}
-        {characterResults && (
-          <CardList
-            title={"Characters"}
-            cards={characterResults}
-            cardComponent={CharacterCard}
+      <button onClick={() => handleSearch(1)}>Search</button>
+      {(maxCount || null) && (
+        <>
+          <div style={{ display: "flex" }}>
+            {loading && <p>Loading...</p>}
+            {characterResults && (
+              <CardList
+                title={"Characters"}
+                cards={characterResults}
+                cardComponent={CharacterCard}
+              />
+            )}
+            {planetResults && (
+              <CardList
+                title={"Planets"}
+                cards={planetResults}
+                cardComponent={PlanetCard}
+              />
+            )}
+            {starshipResults && (
+              <CardList
+                title={"Starships"}
+                cards={starshipResults}
+                cardComponent={StarshipCard}
+              />
+            )}
+          </div>
+          <Pagination
+            current={currentPage}
+            total={maxCount}
+            pageSize={10}
+            onChange={handleSearch} // Call handleSearch when the page number changes
           />
-        )}
-        {planetResults && (
-          <CardList
-            title={"Planets"}
-            cards={planetResults}
-            cardComponent={PlanetCard}
-          />
-        )}
-        {starshipResults && (
-          <CardList
-            title={"Starships"}
-            cards={starshipResults}
-            cardComponent={StarshipCard}
-          />
-        )}
-      </div>
+        </>
+      )}
     </>
   );
 };
